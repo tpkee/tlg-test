57388625673aa3b82d2c6255849eedcc7b991071
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-21 21:49:23 +0100

    refactor: bind all props to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,6 @@
-export default function Button ({ children, onClick }) {
+export default function Button (props) {
   return (
-    <button onClick={onClick} className="px-2.5 py-1.5 border hover:bg-slate-100 active:bg-slate-200 rounded">
-      {children}
+    <button {...props} className="px-2.5 py-1.5 border hover:bg-slate-100 active:bg-slate-200 rounded">
     </button>
   )
 }
