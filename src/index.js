e4878f6e35427e0b48b679f3b9b4d365aec3aa6c
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-21 21:19:15 +0100

    chore: removed unused files

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import Test from "./pages/test";
 import ErrorPage from "./error-page";
 
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
   {
@@ -30,8 +29,3 @@ root.render(
         <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
