c077397078802ef5fa6a129622bf9d9e47113159
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-21 21:46:42 +0100

    feat: add input component

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,4 +1,5 @@
 import Button from '../components/Button';
+import Input from '../components/Input';
 
 export default function Test() {
   return (
@@ -10,6 +11,18 @@ export default function Test() {
         <Button onClick={() => console.log('u pressed me')}>
           Press me!
         </Button>
+      </div>
+      <div>
+        <h3>
+          Input (no label)
+        </h3>
+        <Input placeholder="Hello there!" />
+        <h3>
+          Input (with label)
+        </h3>
+        <Input disabled placeholder="Also disabled">
+          Label
+        </Input>
       </div>
     </div>
   )
