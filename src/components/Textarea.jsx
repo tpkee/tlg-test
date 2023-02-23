12dbfe4e2fcbca0cca83aebd55660bbc5f50c67e
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-23 11:59:33 +0100

    feat: add textarea component; fix: removed selected from option

---
+++ src/components/Textarea.jsx
@@ -0,0 +1,10 @@
+export default function Textarea (props) {
+  const { children } = props
+  const customProps = { ...props, children: null } // I don't want to bind the children to the input tag but use it as a label
+  return (
+    <label className="flex flex-col">
+      {children}
+      <textarea {...customProps} className="px-2.5 py-1.5 border border-slate-200 outline-none disabled:opacity-40 required:invalid:border-red-500 focus:outline-slate-200 rounded" />
+    </label>
+  )
+}
