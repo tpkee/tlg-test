b649d36fbe09585c2a36b9f240a11d5b855901ed
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-23 10:29:57 +0100

    feat: add card product

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,10 +1,14 @@
-export default function CardProduct ({title, listImages, price}) {
+export default function CardProduct ({title, image, price}) {
+  const img = image || 'https://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-md.png'
   return (
-    <div>
-      <picture>
-        <source srcSet={listImages}   width="800" height="800" />
-      </picture>
-      {listImages}
+    <div className="max-w-xs border border-slate-200 rounded p-3.5 space-y-2">
+      <img src={img} alt="" className="rounded h-32 w-full object-cover" />
+      <h6 className="font-bold text-lg truncate">
+        {title}
+      </h6>
+      <span className="text-slate-700 font-medium">
+        {price}&euro;
+      </span>
     </div>
   )
 }
