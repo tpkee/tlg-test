adf60e8f75c3f3874bea8a9a9448d9feb3fa7ebd
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-23 11:50:33 +0100

    fix: add lazy loading to CardProduct's image

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -2,7 +2,7 @@ export default function CardProduct ({title, image, price}) {
   const img = image || 'https://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-md.png'
   return (
     <div className="max-w-xs border border-slate-200 rounded p-3.5 space-y-2">
-      <img src={img} alt="" className="rounded h-32 w-full object-cover" />
+      <img src={img} alt="" className="rounded h-32 w-full object-cover" loading="lazy" />
       <h6 className="font-bold text-lg truncate">
         {title}
       </h6>
