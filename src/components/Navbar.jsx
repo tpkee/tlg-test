d0c66c8fe1da222d36802dacf83ef1471d8dcfd9
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-23 13:39:01 +0100

    feat: add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+import { logout } from '../auth/auth';
+
+import Button from "./Button";
+
+export default function Navbar () {
+  return (
+    <>
+      <div className="fixed top-0 left-0 bg-white w-full  border border-slate-200">
+        <div className="flex justify-center items-center gap-x-3 p-5">
+          <NavLink to="/" className={({ isActive }) =>
+              isActive ? 'underline' : ''
+            }>
+            Products
+          </NavLink>
+          <NavLink to="/contact" className={({ isActive }) =>
+              isActive ? 'underline' : ''
+            }>
+            Contact
+          </NavLink>
+
+          <Button onClick={() => logout()}>
+            Logout
+          </Button>
+        </div>
+      </div>
+      <Outlet className="p-10" />
+    </>
+  )
+}
