72bcbb616f4826bf9dc030885f45381d0608956b
tpkee <89136677+tpkee@users.noreply.github.com>
2023-02-23 12:27:03 +0100

    feat: added wishlist button

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,7 +1,9 @@
-export default function CardProduct ({title, image, price}) {
+import Button from "./Button"
+
+export default function CardProduct ({title, image, price, isWishlisted, onClick}) {
   const img = image || 'https://www.clker.com/cliparts/f/Z/G/4/h/Q/no-image-available-md.png'
   return (
-    <div className="max-w-xs border border-slate-200 rounded p-3.5 space-y-2">
+    <div className="max-w-xs border border-slate-200 rounded p-3.5 space-y-2 flex flex-col">
       <img src={img} alt="" className="rounded h-32 w-full object-cover" loading="lazy" />
       <h6 className="font-bold text-lg truncate">
         {title}
@@ -9,6 +11,9 @@ export default function CardProduct ({title, image, price}) {
       <span className="text-slate-700 font-medium">
         {price}&euro;
       </span>
+      <Button onClick={onClick}>
+        {isWishlisted ? 'Remove from wishlist' : 'Add to wishlist'}
+      </Button>
     </div>
   )
 }
